744e845dcc9a4b0ae98782bd2178f3a2aceb167e
Allan Ly <allanvly@gmail.com>
2018-03-01 19:54:38 -0800

    some js

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-console.log(moment('12/13/1901', 'DD/MM/YYYY').format('ddd MMM, DD, YYYY'))
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyCJ-ReRnQoo-srdudik-rfey-HSrqLXK3U",
-  authDomain: "whatever-first.firebaseapp.com",
-  databaseURL: "https://whatever-first.firebaseio.com",
-  projectId: "whatever-first",
+    apiKey: "AIzaSyCJ-ReRnQoo-srdudik-rfey-HSrqLXK3U",
+    authDomain: "whatever-first.firebaseapp.com",
+    databaseURL: "https://whatever-first.firebaseio.com",
+    projectId: "whatever-first",
   storageBucket: "whatever-first.appspot.com",
   messagingSenderId: "110419074122"
 }
+
 firebase.initializeApp(config)
 
 var database = firebase.database()
@@ -16,17 +16,28 @@ var database = firebase.database()
 var users = database.ref('/users')
 
 $('#submitBTN').on('click', function () {
-  var name = $('#name').val().trim()
-  var email = $('#email').val().trim()
-  var number = parseInt($('#number').val().trim())
-
+    var name = $('#name').val().trim()
+    var role = $('#role').val().trim()
+    var startDate = $('#startDate').val()
+    var monthsWorked = $('#monthsWorked')
+  var monthlyRate = $('#monthlyRate')
+  var totalBilled = $('#totalBilled')
+  
   users.push({
-    name: name,
-    email: email,
-    number: number
-  })
+      name: name,
+    role : role,
+    Start_Date : startDate,
+    Months_Worked : monthsWorked,
+    Monthly_Rate : monthlyRate,
+    Total_Billed : totalBilled
+
 })
 
-users.orderByChild('number').limitToLast(1).on('child_added', function (snap) {
-  console.log(snap.val())
+
 })
+
+users.orderByChild('number').on('child_added', function (snap) {
+    console.log(snap.val())
+})
+
+console.log(moment(startDate, 'DD/MM/YYYY').format('MM/DD/YY'))
